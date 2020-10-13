935af2c8a68340b1f05b399510c9047279dc62de
thetpainghtut <thetpainghtut.bf@gmail.com>
2020-10-13 12:01:50 +0630

    js day-1 => introduction and basic knowledge of programming

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+alert('hello');
+
+console.log('Hello JS');
+
+var condition = confirm('Are you sure'); // ok-> true, cancel-> false
+
+var data = prompt('Accept value from input'); // accept data from that input
